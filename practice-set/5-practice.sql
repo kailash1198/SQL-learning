48ac8fa7085e2b0794d1481040d83b73b17fc739
kailash kumar <kailash414243@gmail.com>
2024-07-08 19:36:14 +0530

    update command practice

---
+++ practice-set/5-practice.sql
@@ -0,0 +1,4 @@
+
+SELECT* FROM table_one;
+
+UPDATE table_one SET `Id_number`=1 WHERE Id_number = 999;
---
+++ practice-set/6-practice.sql
@@ -0,0 +1,3 @@
+SELECT * FROM table_one;
+
+UPDATE table_one SET name = 'Kailash Kumar' WHERE `Id_number` = 1;
