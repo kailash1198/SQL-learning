6fb183c25f8636747ed2489b176697f2618c3ae3
kailash kumar <kailash414243@gmail.com>
2024-07-23 00:17:55 +0530

    DML basic practice

---
+++ 10-DML.sql
@@ -0,0 +1,14 @@
+-- DML - Data manipulation language
+
+-- Command for DML
+-- 1. INSERT INTO command :
+SELECT* FROM student_records.students;
+
+INSERT INTO student_records.students(student_id, first_name,last_name) VALUES(20, 'Aryan', 'SIngh');
+
+CREATE TABLE kailash_team(team_id INT PRIMARY KEY, team_name VARCHAR(50), joining_year DATE);
+SELECT* from kailash_team;
+insert into kailash_team VALUES(1, 'Kailash Kumar',1011);
+insert into kailash_team VALUES(2, 'Aryan Kumar',1012);
+insert into kailash_team VALUES(3, 'Shekhar Kumar',1211);
+insert into kailash_team VALUES(4, 'Mohan Kumar',1011);
