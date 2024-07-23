153d27fc005e9d9de371b5d66b661c50e79facce
kailash kumar <kailash414243@gmail.com>
2024-07-24 01:17:26 +0530

    DML practice

---
+++ practice-set/12-practice.sql
@@ -0,0 +1,7 @@
+SELECT* FROM kailash_team;
+
+INSERT into kailash_team(team_id, team_name, joining_year) values(8, 'Laxmi singh', 1015);
+
+UPDATE kailash_team set team_name = 'Ravi Kumar' WHERE team_id=1;
+
+DROP Table kailash_team;
